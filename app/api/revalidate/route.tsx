0d91fd1c6ddca46ea3f5ea87ec61629b3920598c
David Levai <david@davidlevai.com>
2023-09-23 18:39:35 +0200

    next 13.4.19 might work then. smh...

--- app/api/revalidate/route.tsx
+++ app/api/revalidate/route.tsx
@@ -23,9 +23,9 @@ export async function POST(req: NextRequest) {
     // If the `_type` is `page`, then all `client.fetch` calls with
     // `{next: {tags: ['page']}}` will be revalidated
     revalidateTag(body._type);
-    // if (body.slug) {
-    //   revalidateTag(`${body._type}:${body.slug}`);
-    // }
+    if (body.slug) {
+      revalidateTag(`${body._type}:${body.slug}`);
+    }
 
     return NextResponse.json({
       status: 200,

1a174e9e4f4478f2fa6aa45c0b986326da14e914
David Levai <david@davidlevai.com>
2023-09-24 21:57:53 +0200

    refactor layouts

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,5 +1,11 @@
+import siteMetadata from "@/lib/metadata";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Analytics } from "@/components/analytics";
+import { BackTopButton } from "@/components/back-to-top";
 import Footer from "@/components/footer";
+import { ThemeProvider } from "@/components/theme-provider";
 import { TopNav } from "@/components/top-nav";
 
 interface RootLayoutProps {
@@ -8,19 +14,31 @@ interface RootLayoutProps {
 
 export default async function SiteLayout({ children }: RootLayoutProps) {
   return (
-    <>
-      <a
-        className="absolute left-0 top-0 z-[9999] m-3 block -translate-y-96 overflow-hidden bg-white p-3 text-2xl text-black transition focus:translate-y-0"
-        href="#main-content"
-        aria-label="Skip to Content"
-      >
-        Skip to Content
-      </a>
-      <TopNav />
-      <main className={cn("mb-8 mt-20")} id="main-content">
-        {children}
-      </main>
-      <Footer />
-    </>
+    <ThemeProvider
+      storageKey="productengineerjobs-theme"
+      attribute="class"
+      defaultTheme={siteMetadata.defaultTheme}
+      enableSystem={false}
+    >
+      <body className="min-h-screen bg-gradient-to-b from-slate-100 to-white antialiased dark:bg-gradient-to-b dark:from-gray-900 dark:to-gray-800">
+        <TooltipProvider>
+          <a
+            className="absolute left-0 top-0 z-[9999] m-3 block -translate-y-96 overflow-hidden bg-white p-3 text-2xl text-black transition focus:translate-y-0"
+            href="#main-content"
+            aria-label="Skip to Content"
+          >
+            Skip to Content
+          </a>
+          <TopNav />
+          <main className={cn("mb-8 mt-20")} id="main-content">
+            {children}
+            <Analytics />
+          </main>
+          <Footer />
+        </TooltipProvider>
+        <BackTopButton />
+        <Toaster />
+      </body>
+    </ThemeProvider>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,25 +3,11 @@ import "./globals.css";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import siteMetadata, { BASE_URL } from "@/lib/metadata";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { Analytics } from "@/components/analytics";
-import { BackTopButton } from "@/components/back-to-top";
-import { ThemeProvider } from "@/components/theme-provider";
-
 const inter = Inter({ subsets: ["latin"], display: "swap", variable: "--font-inter" });
 
 export const metadata: Metadata = {
-  metadataBase: new URL(BASE_URL),
-  title: siteMetadata.title,
-  description: siteMetadata.description,
-  alternates: {
-    canonical: "./",
-    types: {
-      "application/rss+xml": `${BASE_URL}/feed.xml`,
-    },
-  },
+  title: "ProductEngineerJobs.co",
+  description: "ProductEngineerJobs.co",
 };
 
 interface RootLayoutProps {
@@ -31,19 +17,7 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" className={`${inter.variable}`} suppressHydrationWarning>
-      <body className="min-h-screen bg-gradient-to-b from-slate-100 to-white antialiased dark:bg-gradient-to-b dark:from-gray-900 dark:to-gray-800">
-        <ThemeProvider
-          storageKey="productengineerjobs-theme"
-          attribute="class"
-          defaultTheme={siteMetadata.defaultTheme}
-          enableSystem={false}
-        >
-          <TooltipProvider>{children}</TooltipProvider>
-          <BackTopButton />
-          <Toaster />
-        </ThemeProvider>
-      </body>
-      <Analytics />
+      {children}
     </html>
   );
 }
